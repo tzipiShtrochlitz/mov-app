5b09e54331b7105e02d2dbf0ce56117e94e888b8
miriamsh <m0527171443@gmail.com>
2022-06-20 13:18:47 +0300

    nake the project ready

--- src/App.js
+++ src/App.js
@@ -1,23 +1,94 @@
-import logo from './logo.svg';
-import './App.css';
+import {BrowserRouter as Router, Switch,Route} from 'react-router-dom';
+import Homeone from './pages/Homeone';
+import Movie from './pages/Movie';
+import MovieDetails from './pages/MovieDetails';
+import TvSeries from './pages/TvSeries';
+import UpcomingMovie from './components/homeone/UpcomingMovie';
+import { useEffect } from "react"
+import $ from "jquery";
+import Pricing from './pages/Pricing';
+import Blogs from './pages/Blogs';
+import BlogDetails from './pages/BlogDetails';
+import ContactPage from './pages/ContactPage';
+import { SignIn } from './pages/sign-in';
+import { SignUp } from './pages/sign-up';
+
+
 
 function App() {
+
+  
+  useEffect(()=>{
+    function preloader() {
+      $('#preloader').delay(0).fadeOut();
+    };
+    $(window).on('load', function () {
+      preloader();
+      
+     
+    });
+  })
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <Router>
+       
+        <Switch>
+          <Route exact={true} path="/">
+            <Homeone/>
+          </Route>
+
+          {/* <Route exact={true} path="/index-2">
+            <HomeTwo/>
+          </Route> */}
+
+          <Route exact={true} path="/upcoming-movie">
+            <UpcomingMovie/>
+          </Route>
+          
+          <Route exact={true} path="/sign-in">
+            <SignIn/>
+          </Route>
+
+          <Route exact={true} path="/sign-up">
+            <SignUp/>
+          </Route>
+
+          <Route exact={true} path="/movie">
+            <Movie/>
+          </Route>
+          <Route exact={true} path="/movie-details">
+            <MovieDetails/>
+
+          </Route>
+          <Route exact={true} path="/tv-show">
+            <TvSeries/>
+
+          </Route>
+          <Route exact={true} path="/pricing">
+            <Pricing/>
+
+          </Route>
+          <Route exact={true} path="/blog">
+            <Blogs/>
+
+          </Route>
+          <Route exact={true} path="/blog-details">
+            <BlogDetails/>
+
+          </Route>
+         
+          <Route exact={true} path="/contact">
+            <ContactPage/>
+
+          </Route>
+
+
+        </Switch>
+        
+     </Router>
+     
+      
+        
     </div>
   );
 }
